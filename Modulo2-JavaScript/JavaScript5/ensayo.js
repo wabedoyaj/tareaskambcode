226ece6260e8def2aa4e701d8758328bfbee4760
wilsonjs <wilson.bedoyajs@gmail.com>
2024-03-18 19:09:35 -0500

    uso de nodejs

---
+++ Modulo2-JavaScript/JavaScript5/ensayo.js
@@ -0,0 +1,13 @@
+// console.log("EJERCICIO 3");
+// for (var i = 1; i<= 10; i++) {
+//     console.log("Tabla de multiplicar del "+i);
+//     for (var  j = 1; j <=10 ; j++) {
+//         var result = i*j;
+//         console.log(i + " x " + j + " = " + result);       
+//     }
+// }
+function saludar(nombre) {
+    console.log("¡Hola, " + nombre + "!");
+}
+
+saludar("María");
